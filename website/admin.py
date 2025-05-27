46600457dfc4b8148d29f1ebbff4531710662f6c
onatuysal <onat.uysal@std.yeditepe.edu.tr>
2025-05-27 22:24:47 +0300

    değişiklikler eklendi

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-
+from .models import Product
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):  
+    list_display = ("name", "category", "quantity", "price", "added_date")
+    search_fields = ("name", "category")
 # Register your models here.
--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100, default="Technology")
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    added_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 # Create your models here.

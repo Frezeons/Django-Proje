25b2f725149caad5ac0ae2db57218ef1e1b0e653
Mahmut Gungor <mahmutcan_gungor@hotmail.com>
2025-05-29 02:10:19 +0300

    Ürünleri silme fonksiyonu getirildi.

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .views import urun_listesi
 
 urlpatterns = [
-    path('urunler/', urun_listesi, name='urun_listesi'),
-
+    path('urunler/', views.urun_listesi, name='urun_listesi'),
+    path('urunler/sil/<int:pk>/', views.urun_sil, name='urun_sil'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 
@@ -6,3 +6,9 @@ from .models import Product
 def urun_listesi(request):
     urunler = Product.objects.select_related('depo').all()
     return render(request, 'urun_listesi.html', {'urunler': urunler})
+
+@login_required
+def urun_sil(request, pk):
+    urun = get_object_or_404(Product, pk=pk)
+    urun.delete()
+    return redirect('urun_listesi')
